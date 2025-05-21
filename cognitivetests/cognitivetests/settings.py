0e55e32637afce81235289053ca64e58065d5da6
egor <egor@mail.africa>
2025-05-21 16:30:04 +0300

    new database + some links in header

--- cognitivetests/cognitivetests/settings.py
+++ cognitivetests/cognitivetests/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'cognitivetests.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cognitivetests',
+        'USER': 'cognitivetests',
+        'PASSWORD': 'cognitivetests',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
