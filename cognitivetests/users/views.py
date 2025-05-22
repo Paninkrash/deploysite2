07f78e21c4592792d0189c3f13948ed1118ef9f4
egor <egor@mail.africa>
2025-05-22 14:19:50 +0300

    закончил с пользователями и сделал дизайн

--- cognitivetests/users/views.py
+++ cognitivetests/users/views.py
@@ -1,17 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth, messages
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from .forms import UserLoginForm, UserRegistrationForm, ProfileForm
+from django.contrib.auth.decorators import login_required
 
 
 def login(request):
-    return render(request, 'users/login.html')
-
+    if request.method == 'POST':
+        form = UserLoginForm(data = request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username = username, password = password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:test_list'))
+    else:
+        form = UserLoginForm()
+    return render(request, 'users/login.html', {'form': form})
 
 def registration(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data = request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            messages.success(request, f'{user.username}, успешно зарегестрирован!',)
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/registration.html', {'form': form})
 
 
+
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files = request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Профиль был успешно изменен!')
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance = request.user)
+
     return render(request, 'users/profile.html')
+    
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:popular_list'))
